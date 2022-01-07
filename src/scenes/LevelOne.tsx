46b423224834c8358ea072b267955985d1dcd276
leavemydogalone <niklasport8*@gmail.com>
2022-01-06 21:38:45 -0800

    uhhh

--- src/scenes/LevelOne.tsx
+++ src/scenes/LevelOne.tsx
@@ -2,29 +2,52 @@ import React, { useState, useEffect } from "react";
 
 export default function LevelOne() {
   const [letters, setLetters] = useState<String[]>([]);
-
   const [inputValue, setInputValue] = useState<string>("");
-  // setting the value of the input to a blank string after a letter has been
+  // setting the value of the input to an empty string after a letter has been
   // entered will prevent multiple letters from being added in each position
   // of the letters array
 
+  const [inputtable, setInputtable] = useState(true);
+
+  //   i think it is something to do with this not working correctly
   function addLetter(input: string): void {
-    setLetters([...letters, input]);
+    const newLetters = [...letters, input];
+    setLetters(newLetters);
   }
+
+  const scriptString =
+    "Welcome I know that you are eager to begin, but first I will need you to tell me your name...";
+
+  useEffect(() => {
+    for (let i = 1; i < scriptString.length - 1; i++) {
+      setTimeout(() => {
+        addLetter(scriptString[i - 1]);
+      }, i * 200);
+      if (i === scriptString.length - 1) setInputtable(true);
+      //   console.log(letters);
+    }
+  }, []);
 
   return (
     <div className="levelOne">
-      <input
-        className="input"
-        type="text"
-        value={inputValue}
-        onChange={(e) => {
-          addLetter(e.target.value);
-        }}
-      />
+      {inputtable && (
+        <input
+          className="input"
+          id="levelOneInput"
+          type="text"
+          value={inputValue}
+          // value is set to "" of inputValue after so that
+          // the previous letters do not stay in the input element
+          onChange={(e) => {
+            addLetter(e.target.value);
+          }}
+        />
+      )}
       <div className="nameDisplay">
-        {letters.map((thing) => (
-          <div className="letter-box">{thing}</div>
+        {letters.map((thing, index) => (
+          <div key={index} className="letter-box">
+            {thing}
+          </div>
         ))}
       </div>
     </div>
