b24e807201ea6a4f25608f9d2809ef0dc31a8e4c
leavemydogalone <niklasport8*@gmail.com>
2022-01-07 19:07:10 -0800

    cool fade in for letters

--- src/scenes/LevelOne.tsx
+++ src/scenes/LevelOne.tsx
@@ -7,29 +7,38 @@ export default function LevelOne() {
   // entered will prevent multiple letters from being added in each position
   // of the letters array
 
-  const [inputtable, setInputtable] = useState(true);
-
-  //   i think it is something to do with this not working correctly
-  //   function addLetter(input: string): void {
-
-  //     setLetters([...letters, input]);
-  //   }
+  const [inputtable, setInputtable] = useState(false);
 
   const scriptString =
-    "Welcome I know that you are eager to begin, but first I will need you to tell me your name...";
+    "Welcome. I know that you are eager to begin, but first I will need your name.";
 
   useEffect(() => {
-    for (let i = 1; i < scriptString.length - 1; i++) {
+    for (let i = 1; i < scriptString.length + 1; i++) {
       setTimeout(() => {
         setLetters((prevLetters) => [...prevLetters, scriptString[i - 1]]);
-      }, i * 200);
-      if (i === scriptString.length - 1) setInputtable(true);
-      //   console.log(letters);
+      }, i * 100);
     }
   }, []);
 
+  useEffect(() => {
+    if (letters.length === scriptString.length) setInputtable(true);
+
+    console.log(letters.length, scriptString.length);
+  }, [letters]);
+
   return (
     <div className="levelOne">
+      <div className="letterDisplay nameDisplay">
+        {letters.map((thing, index) => (
+          <>
+            <div key={index} className="letter-box">
+              {thing}
+            </div>
+            {thing === "." && <br key={index + 100} />}
+          </>
+        ))}
+      </div>
+
       {inputtable && (
         <input
           className="input"
@@ -43,13 +52,6 @@ export default function LevelOne() {
           }}
         />
       )}
-      <div className="nameDisplay">
-        {letters.map((thing, index) => (
-          <div key={index} className="letter-box">
-            {thing}
-          </div>
-        ))}
-      </div>
     </div>
   );
 }
