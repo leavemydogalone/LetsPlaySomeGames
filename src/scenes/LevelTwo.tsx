273da7f39a2de56972ec9bd862ef25adb134f25a
leavemydogalone <niklasport88@gmail.com>
2022-01-27 20:51:15 -0800

    not perfect but it works

--- src/scenes/LevelTwo.tsx
+++ src/scenes/LevelTwo.tsx
@@ -1,34 +1,27 @@
-import React, { MouseEventHandler, useEffect, useState } from "react";
+import React { useState } from "react";
 
 export default function LevelTwo() {
-  const [mousePositionX, setMousePositionX] = useState<number>(0);
-  const [isDraggerMoving, setIsDraggerMoving] = useState(false);
+  //   const [mousePositionX, setMousePositionX] = useState<number>(0);
+  const [originX, setOriginX] = useState<number>(0);
+  const [isDown, setIsDown] = useState(false);
+  const [draggerPositionX, setDraggerPositionX] = useState<number>();
 
-  const draggerContainer =
-    document.querySelector<HTMLDivElement>(".draggerContainer");
-
-  useEffect(() => {
-    if (isDraggerMoving)
-      window.addEventListener("mousemove", handleDraggerMove);
-    return () => window.removeEventListener("mousemove", handleDraggerMove);
-  }, [isDraggerMoving]);
-
-  function relativeCoords(event: any): { x: number } {
-    var bounds = event.target.getBoundingClientRect();
-    var x = event.clientX - bounds.left;
-
-    return { x: x };
-  }
-
-  function handleDraggerMove(e: MouseEvent): void {
-    if (draggerContainer !== null) {
-      draggerContainer.style.transform =
-        "translateX(" + relativeCoords(e).x + "px";
-    }
+  function handleWin() {
+    console.log("you win");
   }
 
   return (
-    <div className="levelTwo">
+    <div
+      className="levelTwo"
+      onMouseUp={() => setIsDown(false)}
+      onMouseLeave={() => setIsDown(false)}
+      onMouseMoveCapture={(e) => {
+        if (isDown) {
+          if (e.clientX - originX > 1 && e.clientX - originX < 200)
+            setDraggerPositionX(e.clientX - originX);
+        }
+      }}
+    >
       <div className="top">
         <h2 className="title first">So you can spell...</h2>
         <h2 className="title second">For your second test...</h2>
@@ -36,7 +29,7 @@ export default function LevelTwo() {
       </div>
       <div className="bottom">
         <div className="track">
-          <div className="keyContainer">
+          <div className="keyContainer" onClick={handleWin}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
@@ -48,11 +41,15 @@ export default function LevelTwo() {
               <path d="M22 19h-6v-4h-2.68c-1.14 2.42-3.6 4-6.32 4-3.86 0-7-3.14-7-7s3.14-7 7-7c2.72 0 5.17 1.58 6.32 4H24v6h-2v4zm-4-2h2v-4h2v-2H11.94l-.23-.67C11.01 8.34 9.11 7 7 7c-2.76 0-5 2.24-5 5s2.24 5 5 5c2.11 0 4.01-1.34 4.71-3.33l.23-.67H18v4zM7 15c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3zm0-4c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1z" />
             </svg>
           </div>
-          <div
-            className="draggerContainer"
-            onClick={() => setIsDraggerMoving(!isDraggerMoving)}
-          >
-            <div className="dragger"></div>
+          <div className="draggerContainer" style={{ left: draggerPositionX }}>
+            <div
+              className="dragger"
+              onMouseDown={(e) => {
+                setIsDown(true);
+                setOriginX(e.clientX);
+                console.log("mouse is clicked and originx is" + originX);
+              }}
+            ></div>
           </div>
         </div>
       </div>
