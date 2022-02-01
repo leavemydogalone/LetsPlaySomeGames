1fa25851906aee564c72e8e702109b374ff75523
leavemydogalone <niklasport88@gmail.com>
2022-01-31 19:05:37 -0800

    yea this one is better

--- src/scenes/LevelTwo.tsx
+++ src/scenes/LevelTwo.tsx
@@ -11,6 +11,9 @@ export default function LevelTwo() {
     console.log("you win");
   }
 
+  const draggerContainer: HTMLElement | null =
+    document.querySelector(".draggerContainer");
+
   return (
     <div
       className="levelTwo"
@@ -18,8 +21,7 @@ export default function LevelTwo() {
       onMouseLeave={() => setIsDown(false)}
       onMouseMoveCapture={(e) => {
         if (isDown) {
-          if (e.clientX - originX > 1 && e.clientX - originX < 200)
-            setDraggerPositionX(e.clientX - originX);
+          setDraggerPositionX(originX + e.clientX);
         }
       }}
     >
@@ -42,15 +44,16 @@ export default function LevelTwo() {
               <path d="M22 19h-6v-4h-2.68c-1.14 2.42-3.6 4-6.32 4-3.86 0-7-3.14-7-7s3.14-7 7-7c2.72 0 5.17 1.58 6.32 4H24v6h-2v4zm-4-2h2v-4h2v-2H11.94l-.23-.67C11.01 8.34 9.11 7 7 7c-2.76 0-5 2.24-5 5s2.24 5 5 5c2.11 0 4.01-1.34 4.71-3.33l.23-.67H18v4zM7 15c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3zm0-4c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1z" />
             </svg>
           </div>
-          <div className="draggerContainer" style={{ left: draggerPositionX }}>
-            <div
-              className="dragger"
-              onMouseDown={(e) => {
-                setIsDown(true);
-                setOriginX(e.clientX);
-                console.log("mouse is clicked and originx is" + originX);
-              }}
-            ></div>
+          <div
+            onMouseDown={(e) => {
+              setIsDown(true);
+              setOriginX(draggerContainer!.offsetLeft - e.clientX);
+              console.log("mouse is clicked and originx is" + originX);
+            }}
+            className="draggerContainer"
+            style={{ left: draggerPositionX }}
+          >
+            <div className="dragger"></div>
           </div>
         </div>
       </div>
