32945cd1daa61819b2da1f706f8ef2c3bdd84251
leavemydogalone <niklasport88@gmail.com>
2022-02-05 19:56:53 -0800

    start spin added

--- src/scenes/LevelThree.tsx
+++ src/scenes/LevelThree.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SpinBar from "../components/SpinBar";
 
 export default function LevelThree() {
   const [start, setStart] = useState(false);
   const standInString = "kykldashiys";
+
+  useEffect(() => {
+    const startSpin = setTimeout(() => {
+      setStart(true);
+    }, 10000);
+  }, []);
 
   const handleWin = () => {
     console.log("you win");
